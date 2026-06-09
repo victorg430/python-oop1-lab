ba14a458b022a048d3ec656560092343bda5b328
victorg430 <your-github-email@example.com>
2026-06-09 07:47:11 +0300

    python-oop1-lab

---
+++ .github/cofee.py
@@ -0,0 +1,19 @@
+class Coffee:
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
